2e32214e969da603c8bdb924d49f90906c3e570c
Jose Soto <jose@homehero.org>
2016-12-15 15:26:50 -0800

    Added model factories and seeders

--- database/seeds/AdminUserSeeder.php
+++ database/seeds/AdminUserSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class AdminUserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(AdminUserSeeder::class);
-        $this->call(CommentsTableSeeder::class);
+        factory(App\User::class)->create([
+        	'email' => 'admin@test.com'
+        ]);
     }
 }
